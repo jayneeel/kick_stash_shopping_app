df408f1e1ddeee6f2cfee1dfd7c46a493e463d29
Jayneel Kanungo <jayneelkanungo57@gmail.com>
2024-06-06 17:14:51 +0530

    - header completed

--- lib/main.dart
+++ lib/main.dart
@@ -14,8 +14,14 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: "KickStash Shopping App",
       theme: ThemeData(
-        fontFamily: "Lato"
-      ),
+          fontFamily: "Lato",
+          colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromRGBO(254, 206, 1, 1)),
+          inputDecorationTheme: const InputDecorationTheme(
+              hintStyle: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 16,
+          ),
+          prefixIconColor: Color.fromRGBO(119, 119, 119, 1))),
       home: const HomeScreen(),
     );
   }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,12 +1,46 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
+  final List<String> filters = const [
+    "All",
+    "Adidas",
+    "Nike",
+    "Reebok",
+  ];
+
   @override
   Widget build(BuildContext context) {
+    const border = OutlineInputBorder(
+        borderSide: BorderSide(color: Color.fromRGBO(225, 225, 225, 1)),
+        borderRadius: BorderRadius.horizontal(left: Radius.circular(50)));
+
     return const Scaffold(
-      body: Center(child: Text("KickStash App", style: TextStyle(fontFamily: "Lato"),),),
+      body: SafeArea(
+          child: Column(
+        children: [
+          Row(
+            children: [
+              Padding(
+                padding: EdgeInsets.all(20),
+                child: Text(
+                  "Shoes\nCollection",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 35),
+                ),
+              ),
+              Expanded(
+                  child: TextField(
+                decoration: InputDecoration(
+                    hintText: "Search",
+                    prefixIcon: Icon(Icons.search),
+                    border: border, focusedBorder: border, enabledBorder: border),
+              ))
+            ],
+          ),
+        ],
+      )),
     );
   }
 }
